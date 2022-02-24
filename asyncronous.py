00dae33f817cc62019f41845a6a700b9029bac5c
Franz Kurt <franz.gastring@gmail.com>
2022-02-24 14:42:04 -0300

    Update asyncronous.py

--- asyncronous.py
+++ asyncronous.py
@@ -48,7 +48,7 @@ async def run_sequence(*functions: Awaitable[Any]) -> None:
 	for function in functions:
 		await function
 
-async def run_parallel(*functions: Awaitable) -> None:
+async def run_parallel(*functions: Awaitable[Any]) -> None:
 	await asyncio.gather(*functions)
 
 async def main() -> None:
